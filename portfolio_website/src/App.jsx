063f5502d41dd5ccfa103003072fff8e7f734a58
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-20 18:21:06 +0300

    css files loaded

--- portfolio_website/src/App.jsx
+++ portfolio_website/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import React from "react";
 import { NavBar } from "./Component/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
