ec449a3b4325e9f75b71e28ce1fede7bd26bad7e
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-23 16:22:39 +0300

    use state and effect added for animation

--- portfolio_website/src/Component/Banner.jsx
+++ portfolio_website/src/Component/Banner.jsx
@@ -1,7 +1,10 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg"
 
 export const Banner = () => {
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer"]
   return (
     <div>
       <section className="banner" id="home">
@@ -9,9 +12,12 @@ export const Banner = () => {
           <Row className="align-items-center">
             <Col xs={12} md={6} xl={7}>
               <span className="tagline">Welcome to my Portfolio</span>
-              <h1>{`Hi I'm webcoded`}<span className="wrap">web developer</span></h1>
+              <h1>{`Hi I'm webcoded`}<span className="wrap"> web developer</span></h1>
               <p>Lorem ipsum</p>
-              <button onClick={() => console.log('connect')}>Let's connect </button>
+              <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+            </Col>
+            <Col xs={12} md={6} xl={5} >
+            <img src={headerImg} alt="Header image"></img>
             </Col>
           </Row>
         </Container>
