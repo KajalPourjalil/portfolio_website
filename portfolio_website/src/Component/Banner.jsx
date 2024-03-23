345fa6131c86b5db8f6301a0a9b8b76502aa1dc2
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-23 15:19:34 +0300

    bootstrap icons installed, imported Col and now the code shows

--- portfolio_website/src/Component/Banner.jsx
+++ portfolio_website/src/Component/Banner.jsx
@@ -1,4 +1,5 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
 
 export const Banner = () => {
   return (
@@ -8,7 +9,9 @@ export const Banner = () => {
           <Row className="align-items-center">
             <Col xs={12} md={6} xl={7}>
               <span className="tagline">Welcome to my Portfolio</span>
-              <h1>{`Hi I'm webcoded`}</h1>
+              <h1>{`Hi I'm webcoded`}<span className="wrap">web developer</span></h1>
+              <p>Lorem ipsum</p>
+              <button onClick={() => console.log('connect')}>Let's connect </button>
             </Col>
           </Row>
         </Container>
