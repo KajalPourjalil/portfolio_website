32f1c9f2d8cbf96ae0be52a7ac3b95bd0bcde9b5
KajalPourjalil <rina.skyler@yahoo.com>
2024-03-24 12:10:28 +0300

    Projects comp complete. fixed bug and code shows

--- portfolio_website/src/Component/Projects.jsx
+++ portfolio_website/src/Component/Projects.jsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 
 export const Projects = () => {
   const projects = [
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg1,
+      // imgUrl: projImg1,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg2,
+      // imgUrl: projImg2,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg3,
+      // imgUrl: projImg3,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg4,
+      // imgUrl: projImg4,
     },
     {
       title: "Business Startup",
       description: "Design & Development",
-      imgUrl: projImg5,
-    },
+      // imgUrl: projImg5,
+    }
   ];
 
   return (
@@ -37,6 +37,30 @@ export const Projects = () => {
           <Col>
             <h2>Projects</h2>
             <p>Lorem ipsum</p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tap Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <p>{project.title}</p>;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">loren ipsum</Tab.Pane>
+                <Tab.Pane eventKey="third">lorem ipsum</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
           </Col>
         </Row>
       </Container>
